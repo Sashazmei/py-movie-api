b43cc15e067a72c066e2e332264ecd6ba17ee0c1
Sasha <sasazmej509@gmail.com>
2025-07-15 23:03:12 +0300

    Initial commit: Add cinema project with Movie API

---
+++ cinema_project/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.views.generic import RedirectView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cinema API",
+        default_version='v1',
+        description="API for managing movies in the cinema app",
+        contact=openapi.Contact(email="your@email.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/', permanent=False)),  # 👈 редирект
+    path('admin/', admin.site.urls),
+    path('api/cinema/', include('cinema.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
