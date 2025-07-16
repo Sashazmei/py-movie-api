80c1e6eae426ee2d957415f981b93e11bc1a9a19
Sasha <sasazmej509@gmail.com>
2025-07-16 19:19:36 +0300

    Fix: register Movie model in admin and add API tests

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+# Регистрируем модель Movie в админке
+admin.site.register(Movie)
--- cinema/tests.py
+++ cinema/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Movie
 
-# Create your tests here.
+class MovieAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.movie_data = {
+            'title': 'Test Movie',
+            'year': 2023  # Замените на актуальные поля вашей модели
+        }
+        self.url = reverse('movie-list')  # Убедитесь, что этот URL существует в urls.py
+
+    def test_create_movie(self):
+        response = self.client.post(self.url, self.movie_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_movies(self):
+        Movie.objects.create(**self.movie_data)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)
